e6820ae94b5dd1e42e4240a9a726711f64b0b561
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-07 19:33:46 +0300

    Task24

--- Task024/Program.cs
+++ Task024/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+// Например:
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int SumNumbers (int number)
+{
+    int sum = default;
+    for (int i = 1; i <= number; i++ )
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+Console.WriteLine("Введите цело число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 0)
+{
+    int s = SumNumbers(n);
+    Console.WriteLine($"Сумма чисел от 1 до {n} составляет {s}");
+}
+else 
+{
+  Console.WriteLine("Введите целое положительное число");  
+}
