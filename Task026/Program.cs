de7ca73a3df1b60f01fe49c0b524bd0acedd28ae
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-07 20:13:23 +0300

    Task26

--- Task026/Program.cs
+++ Task026/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт количество цифр в числе.
+// Например: 456 -> 3; 78 -> 2; 89126 -> 5
+
+int CountNumber (int number)
+{
+    int count = default;
+    while(number % 10 > 0)
+    {
+        count = count + 1;
+        number = number / 10;
+    }
+    return count;
+}
+Console.WriteLine("Введите целое положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n >= 0)
+{
+    int c = CountNumber(n);
+    Console.WriteLine($"В числе {n} содержитсся цифр {c}");
+}
+else
+{
+    int n1 = n*(-1);
+    int c = CountNumber(n1);
+    Console.WriteLine($"В числе {n1} содержитсся цифр {c}");
+}
