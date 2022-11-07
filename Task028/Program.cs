05894b785852f698b6f1b34ad0807ae026eaa23b
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-07 21:08:50 +0300

    Task28

--- Task028/Program.cs
+++ Task028/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N.
+// Например: 4 -> 24; 5 -> 120
+
+int Factorial(int num)
+{
+    int p = 1;
+    for (int i = 1; i <= num; i++ )
+    {
+        p = p * i;
+    }
+    return p;
+}
+Console.WriteLine("Введите целое число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n >= 0)
+{
+int f = Factorial(n);
+Console.WriteLine($"Произведение чисел от 1 до {n} равно {f}");
+}
+else 
+{
+  Console.WriteLine("Введите положительное число");  
+}
