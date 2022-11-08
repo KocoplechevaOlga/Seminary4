128c692f752b04cd8e32dd35c73887d775c072c0
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-08 14:37:32 +0300

    Task30

--- Task030/Program.cs
+++ Task030/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+// Например: [1,0,1,1,0,1,0,0]
+
+int[] FillArray(int l)
+{
+    int[] array = new int[l];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(0, 2);
+        //Console.Write(array[i] + ", ");
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+}
+Console.WriteLine("Введите размер массива:");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int [] res = FillArray(length);
+PrintArray(res);
