aa37da72368e764ea6041f07a6ec0bcdbb68fd26
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-11-08 14:58:42 +0300

    OptTask30

--- Task030/Program.cs
+++ Task030/Program.cs
@@ -5,9 +5,10 @@
 int[] FillArray(int l)
 {
     int[] array = new int[l];
+    Random rnd = new Random();
     for(int i = 0; i < array.Length; i++)
     {
-        array [i] = new Random().Next(0, 2);
+        array [i] = rnd.Next(0, 2);
         //Console.Write(array[i] + ", ");
     }
     return array;
